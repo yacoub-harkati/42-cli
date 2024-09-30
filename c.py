af3c79d4d6af5402c7da1a93e83ae9c47cd993a0
Yacoub Harkati <haryacoub@gmail.com>
2024-09-30 19:37:06 +0100

    repeat

--- c.py
+++ c.py
@@ -2,12 +2,7 @@ import pygame as p, Quartz as q, sys as s, time as t
 from PIL import Image as i
 
 def b_i():
-    et = q.CGEventTapCreate(
-        q.kCGSessionEventTap, 
-        q.kCGHeadInsertEventTap, 
-        q.kCGEventTapOptionDefault, 
-        q.kCGEventMaskForAllEvents,
-        lambda pr, t, e, rc: None, None)
+    et = q.CGEventTapCreate(q.kCGSessionEventTap, q.kCGHeadInsertEventTap, q.kCGEventTapOptionDefault, q.kCGEventMaskForAllEvents, lambda pr, t, e, rc: None, None)
     if et:
         rls = q.CFMachPortCreateRunLoopSource(None, et, 0)
         q.CFRunLoopAddSource(q.CFRunLoopGetCurrent(), rls, q.kCFRunLoopCommonModes)
@@ -23,69 +18,47 @@ def d_i_w_f_i(img_p, t_w):
     sc_w, sc_h = sc_info.current_w, sc_info.current_h
     sc = p.display.set_mode((sc_w, sc_h), p.FULLSCREEN)
     p.display.set_caption('L')
-
     img = i.open(img_p)
     img = img.resize((sc_w, sc_h), i.LANCZOS)
     img = img.convert("RGB")
-    md = img.mode
-    sz = img.size
-    dt = img.tobytes()
+    md, sz, dt = img.mode, img.size, img.tobytes()
     srf = p.image.fromstring(dt, sz, md)
-
     b_i()
-    t_wd = ""
-    r = True
-    idle = False
-    last_mouse_pos = p.mouse.get_pos()
-    idle_time = 0
-
+    t_wd, r, idle, last_mouse_pos, idle_time, idle_limit = "", True, False, p.mouse.get_pos(), 0, 5
     while r:
         mouse_pos = p.mouse.get_pos()
         if mouse_pos != last_mouse_pos:
-            idle = False
+            idle, idle_time = False, 0
         else:
             idle_time += p.time.get_ticks() / 1000.0
-
         last_mouse_pos = mouse_pos
-
         if idle_time >= idle_limit:
             idle = True
-
         if idle:
+            sc.fill((0, 0, 0))
         else:
-
+            sc.blit(srf, (0, 0))
         p.display.update()
-
         for ev in p.event.get():
             if ev.type == p.QUIT:
                 p.quit()
                 s.exit()
-
             if ev.type == p.KEYDOWN:
                 ch = ev.unicode
                 if ch.isalnum():
                     t_wd += ch
-
-                if ev.key == p.K_RETURN:
-                    if t_wd == t_w:
-                        u_i()
-                        r = False
-
+                if ev.key == p.K_RETURN and t_wd == t_w:
+                    u_i()
+                    r = False
                 if ev.key == p.K_BACKSPACE:
                     t_wd = t_wd[:-1]
-
                 if ev.key == p.K_ESCAPE:
                     t_wd = ""
-
             if ev.type == p.MOUSEMOTION:
-                idle = False
-                idle_time = 0
-
+                idle, idle_time = False, 0
         t.sleep(0.1)
-
     p.quit()
 
 if __name__ == "__main__":
-    i_p = 'l.png'
-    u_w = '282480'
+    i_p, u_w = 'l.png', '282480'
     d_i_w_f_i(i_p, u_w)
