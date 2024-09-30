539afa9c456da7b9a9bf319f053838520befc2bd
Yacoub Harkati <haryacoub@gmail.com>
2024-09-30 17:44:56 +0100

    a

---
+++ c.py
@@ -0,0 +1,64 @@
+import subprocess
+import pygame as p, Quartz as q, sys as s
+from PIL import Image as i
+
+def b_i():
+    et = q.CGEventTapCreate(
+        q.kCGSessionEventTap, 
+        q.kCGHeadInsertEventTap, 
+        q.kCGEventTapOptionDefault, 
+        q.kCGEventMaskForAllEvents,
+        lambda pr, t, e, rc: None, None)
+    if et:
+        rls = q.CFMachPortCreateRunLoopSource(None, et, 0)
+        q.CFRunLoopAddSource(q.CFRunLoopGetCurrent(), rls, q.kCFRunLoopCommonModes)
+        q.CGEventTapEnable(et, True)
+    q.CGDisplayHideCursor(q.CGMainDisplayID())
+
+def u_i():
+    q.CGDisplayShowCursor(q.CGMainDisplayID())
+
+def run_c():
+    subprocess.Popen(['caffeinate', '-d', '-i', '-m', '-u', '-t', '28000', ';', 'pmset', 'displaysleepnow'], shell=True)
+
+def d_i_w_f_i(img_p, t_w):
+    p.init()
+    sc = p.display.set_mode((0, 0), p.FULLSCREEN)
+    p.display.set_caption('L')
+    img = i.open(img_p)
+    img = img.convert("RGB")
+    md = img.mode
+    sz = img.size
+    dt = img.tobytes()
+    srf = p.image.fromstring(dt, sz, md)
+    b_i()
+    t_wd = ""
+    r = True
+    while r:
+        sc.fill((0, 0, 0))
+        sc.blit(srf, (0, 0))
+        p.display.update()
+        for ev in p.event.get():
+            if ev.type == p.QUIT:
+                p.quit()
+                s.exit()
+            if ev.type == p.KEYDOWN:
+                ch = ev.unicode
+                if ch.isalnum():
+                    t_wd += ch
+                if ev.key == p.K_RETURN:
+                    if t_wd == t_w:
+                        u_i()
+                        r = False
+                if ev.key == p.K_BACKSPACE:
+                    t_wd = t_wd[:-1]
+    p.quit()
+
+if __name__ == "__main__":
+    run_c()
+
+    i_p = 'l.png'
+    
+    u_w = '282480'
+
+    d_i_w_f_i(i_p, u_w)
